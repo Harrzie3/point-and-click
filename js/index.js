5cd90de39f7fcd27bab23a9048342495860c7c5f
Harrzie3 <127936804+Harrzie3@users.noreply.github.com>
2023-03-15 12:54:05 +0100

    3

--- js/index.js
+++ js/index.js
@@ -1,2 +1,3 @@
 document.getElementById("main").appendChild(document.createTextNode("fakka"))
 document.getElementById("mainTitle").innerText = "point and click adventure";
+document.getElementById("mainTitle").style.textAlign = "center";
