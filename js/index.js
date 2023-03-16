2019cddfcbf757c5848e8d805119c0c16036c135
Harrzie3 <127936804+Harrzie3@users.noreply.github.com>
2023-03-16 12:24:18 +0100

    v

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
-document.getElementById("main").appendChild(document.createTextNode("fakka"))
+document.getElementById("main").appendChild(document.createTextNode(""))
 document.getElementById("mainTitle").innerText = "point and click adventure";
 document.getElementById("mainTitle").style.textAlign = "center";
