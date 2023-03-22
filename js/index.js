39e54a598ac067f7d8f73b9cf8b60c307b2a34f6
Harrzie3 <127936804+Harrzie3@users.noreply.github.com>
2023-03-22 10:56:10 +0100

    js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
-document.getElementById("main").appendChild(document.createTextNode(""))
+
 document.getElementById("mainTitle").innerText = "point and click adventure";
 document.getElementById("mainTitle").style.textAlign = "center";
+
+const offsetCharater =16;
+
+const mainCharater = document.getElementById("mainCharacter")
+const gameWindow = document.getElementById("gameWindow")
+
+
+gameWindow.onclick = function(e) {
+   var Rect = gameWindow. getBoundingClientRect();
+   var x = e.clientx - Rect.left;
+   var y = e.clienty - Rect.top;
+   mainCharater.Style.left = x + "px";
+   mainCharater.Style.top = y + "px";
+   // console.log(x)
+}
