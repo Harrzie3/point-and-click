ca6d05029013aa91581a886e20d6535255da6690
Harrzie3 <127936804+Harrzie3@users.noreply.github.com>
2023-03-29 12:59:04 +0200

    yay

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ document.getElementById("mainTitle").style.textAlign = "center";
 
 const offsetCharacter =16;
 
+const sec= 1000;
+
 const mainCharacter = document.getElementById("mainCharacter");
 const gameWindow = document.getElementById("gameWindow");
 const characterAudio = document.getElementById("characterAudio");
@@ -19,22 +21,20 @@ gameWindow.onclick = function(e) {
    mainCharacter.style.left = x - offsetCharacter +"px";
    mainCharacter.style.top = y - offsetCharacter + "px";
     
-
-
-   showSpeech("yes i do")
+   console.log(e.target.id);
 
     switch(e.target.id){
         case "house1":
-            characterAudio.play(); 
-            showSpeech("this is not your house bozo");
+           // characterAudio.play(); 
+            showSpeech(mainCharacterSpeech,characterAudio,"this is not your house bozo");
             break;
         case "house2":
             //sonething insert here
-            showSpeech("this is not your house bozo");
+            showSpeech(mainCharacterSpeech,characterAudio,"this is not your house bozo");
             break;
         case "tree1":
             //sonething insert here
-            showSpeech("yes this tree is green");
+            showSpeech(mainCharacterSpeech,characterAudio,"yes this tree is green");
             break;
         default:
             // do something when it doesn't have a case
@@ -45,13 +45,18 @@ gameWindow.onclick = function(e) {
     // console.log(x);
 }
 
-function showSpeech(dialog){
-    mainCharacterSpeech.style.opacity =1;
-    mainCharacterSpeech.innerHTML = dialog;
+function showSpeech(targetBubble, targetAudio, dialog){
+    targetBubble.style.opacity = 1;
+    targetBubble.innerHTML = dialog;
+    targetAudio.currentTime = 0;
+    targetAudio.play(); 
+    //stop after 4 seconds the dialog bubble and audio
+    setTimeout(hideSpeech, 4 * sec);
 }
 
 function hideSpeech(){
-    mainCharacterSpeech.style.opacity =0;
+    mainCharacterSpeech.style.opacity = 0;
     mainCharacterSpeech.innerHTML = "...";
+  //  characterAudio.pause();
 }
 
