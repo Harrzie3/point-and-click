e71de886a13b75b5cc9ce72a8a0421e9abc5568b
Harrzie3 <127936804+Harrzie3@users.noreply.github.com>
2023-04-05 11:52:53 +0200

    branch

--- js/index.js
+++ js/index.js
@@ -12,14 +12,22 @@ const gameWindow = document.getElementById("gameWindow");
 const characterAudio = document.getElementById("characterAudio");
 const mainCharacterSpeech = document.getElementById("mainCharacterSpeech")
 
+const counterSpeech =document.getElementById("counterSpeech");
+const counterAudio =document.getElementById("counterAudio");
+const counterCharacter =document.getElementById("counterCharacter");
+let tilemapIMG = document.getElementById("tilemapIMG");
+
+let inventory = [];
+const inventoryList = document.getElementById("inventoryList");
 
 gameWindow.onclick = function(e) {
-
-   var Rect = gameWindow.getBoundingClientRect();
-   var x = e.clientX - Rect.left;
-   var y = e.clientY - Rect.top;
+  if (mainCharacterSpeech.style.opacity == 0 && counterSpeech.style.opacity == 0) {
+   var rect = gameWindow.getBoundingClientRect();
+   var x = e.clientX - rect.left;
+   var y = e.clientY - rect.top;
    mainCharacter.style.left = x - offsetCharacter +"px";
    mainCharacter.style.top = y - offsetCharacter + "px";
+  
     
    console.log(e.target.id);
 
@@ -32,14 +40,33 @@ gameWindow.onclick = function(e) {
             //sonething insert here
             showSpeech(mainCharacterSpeech,characterAudio,"this is not your house bozo");
             break;
-        case "tree1":
-            //sonething insert here
-            showSpeech(mainCharacterSpeech,characterAudio,"yes this tree is green");
+        case "boat":
+            if (checkItem("branch")){
+                showSpeech(mainCharacterSpeech,characterAudio,"LETSSS GOOO IT'S FIXED :)");
+                removeItem("branch", "branch")
+            }else{
+                showSpeech(mainCharacterSpeech,characterAudio,"hmmm this boat is broken");
+            }
+        
             break;
+
+            case "tree1":
+                //something insert here
+                showSpeech(mainCharacterSpeech, characterAudio, "dammm its just a tree");
+                setTimeout(function () { counterCharacter.style.opacity = 1; }, 4 * sec);
+                setTimeout(showSpeech, 4 * sec, counterSpeech, counterAudio, "peepeepoopoo");
+                setTimeout(showSpeech, 8 * sec, mainCharacterSpeech, characterAudio, "AYO YOU TALK BITCH O.O");
+                setTimeout(showSpeech, 12 * sec, counterSpeech, counterAudio, " have one of my branches maybe you need it, good luck!");
+                getItem("branch", "branch");
+                setTimeout(function () { counterCharacter.style.opacity = 0; }, 16* sec);
+            break;
+
         default:
             // do something when it doesn't have a case
-            hideSpeech();
+            
+            //hideSpeech();
             break;
+    }
 
     }
     // console.log(x);
@@ -51,12 +78,42 @@ function showSpeech(targetBubble, targetAudio, dialog){
     targetAudio.currentTime = 0;
     targetAudio.play(); 
     //stop after 4 seconds the dialog bubble and audio
-    setTimeout(hideSpeech, 4 * sec);
+    setTimeout(hideSpeech, 4 * sec, targetBubble, targetAudio);
 }
 
-function hideSpeech(){
-    mainCharacterSpeech.style.opacity = 0;
-    mainCharacterSpeech.innerHTML = "...";
-  //  characterAudio.pause();
+function hideSpeech(targetBubble, targetAudio){
+    targetBubble.style.opacity = 0 
+    targetBubble.innerHTML = "...";
+    targetAudio.pause(); 
 }
 
+function getItem(itemName, itemId) {
+    if (!checkItem(itemName)) {
+        inventory.push(itemName);
+        showItem(itemName, itemId);
+    }
+}
+
+function checkItem(item) {
+    return inventory.includes(item);
+}
+
+function showItem(itemName, itemId) {
+    let listItem = document.createElement("li");
+    listItem.id = itemId;
+    listItem.appendChild(document.createTextNode(itemName));
+    inventoryList.appendChild(listItem);
+}
+
+function removeItem(itemName, itemId) {
+    //remove item in Array
+    inventory = inventory.filter(function (newInventory) {
+        return newInventory !== itemName;
+    });
+    //removes list element in HTML
+    document.getElementById(itemId).remove();
+}
+
+function inOrOut(){
+
+}
